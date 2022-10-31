1052ddedf5ff0731f9c9d3655578a14def6c35cd
Shuo Shuo5 Huang <huangshuo5@lenovo.com>
2022-10-31 11:34:46 +0800

    update: base options

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig  } from 'vuepress'
 import theme from "./theme.js";
 
 export default defineUserConfig({
+  base: "/",
   lang: 'zh-CN',
   title: '开发指南',
   description: '文档存储，知识库，笔记，指南',
